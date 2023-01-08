a4e58ffe7d078d7fd0a3e7c0b00293375631392a
Baraa AL-Saqqa <baraa.sk@gmail.com>
2023-01-08 11:14:42 +0200

    Update backfire.bat

--- backfire.bat
+++ backfire.bat
@@ -1,1 +1,8 @@
-php ./backfire
+@echo off && setlocal EnableDelayedExpansion
+
+for %%Z in (%*)do set "_arg_=%%Z" && set/a "_cnt+=1+0" && call set "_arg_[!_cnt!]=!_arg_!")
+
+echo "The argument is: !_arg_!"
+php ./backfire !_arg_!
+
+goto :eof
